cb68aaae28d486a04c37aba8ee840c0990f33e2d
Cristian <cristianreynaldi2@gmail.com>
2026-09-16 13:25:10 +0700

    feat(modul3): tambah routing controller dan view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tentang Kami</title>
+</head>
+<body>
+    <h1>Tentang Kami</h1>
+    <p>Halaman informasi tentang proyek PjBL kami.</p>
+</body>
+</html>
---
+++ resources/views/service.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Layanan</title>
+</head>
+<body>
+    <h1>Layanan</h1>
+    <p>Halaman layanan dari aplikasi PjBL kami.</p>
+</body>
+</html>
